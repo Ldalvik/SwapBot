ad848e41e2e393e83a0bee7eddddbdeda17b9b7f
Julian Osborne <subaruforge@gmail.com>
2022-07-11 09:01:10 -0400

    Initial commit

---
+++ src/Config.java
@@ -0,0 +1,12 @@
+package swap.bot.root;
+
+public class Config {
+    public static String VERSION = "0.1.0";
+    public static String Guild_SWAPBOT = "969809178401050655";
+    public static String Guild_SWAPBOT_Channel_STATUS = "972053387921219584";
+    public static String Guild_SWAPBOT_Channel_STOCKPRICES = "976361208670224414";
+    public static String Guild_SWAPBOT_Channel_DIPSANDRISES = "978893750782791680";
+
+    public static String Bot_MISA = "584160547272917015";
+
+}
